de91c27a33b143ef885be757bc921b64ee59438d
devtomas2003 <54170951+devtomas2003@users.noreply.github.com>
2019-11-01 16:22:59 +0000

    Inicio da Programação com TDD
    Preparação do ambiente para TDD e criação da variavel ambiente PORT

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 'use strict';
-const express = require("express");
-const routes = require("./routes");
+const app = require('./app');
+require('dotenv').config();
 require('./database');
-const app = express();
-app.use(express.json());
-app.use(routes);
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
