ec3aa309b029b993b2fc0c5645018865441a8906
devtomas2003 <54170951+devtomas2003@users.noreply.github.com>
2020-02-04 17:23:45 +0000

    Permitindo autenticações offline

--- src/controllers/offlineController.js
+++ src/controllers/offlineController.js
@@ -1,5 +1,6 @@
 'use strict';
-const { Disciplinas, Horarios, Profs, Salas, Tempos, Turmas, DiasDaSemana, versions } = require('../models');
+const { Disciplinas, Horarios, Profs, Salas, Tempos, Turmas, DiasDaSemana, Auth, versions, Utilizadores } = require('../models');
+const crypto = require('crypto');
 require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 module.exports = {
     async getOffline(req, res){
@@ -10,6 +11,7 @@ module.exports = {
         const getSalas = await Salas.findAll({ raw: true });
         const getTempos = await Tempos.findAll({ raw: true });
         const getTurmas = await Turmas.findAll({ raw: true });
+
         return res.status(200).json({
             "disciplinas": getDisciplinas,
             "horarios": getHorarios,
@@ -21,9 +23,14 @@ module.exports = {
         });
     },
     async getHash(req, res){
+        const authHeader = req.headers.authorization;
+        const [, token] = authHeader.split(" ");
+        const verifyToken = await Auth.findOne({ where: { hash: token }});
+        const getUserCheckIntegrity = await Utilizadores.findByPk(verifyToken.idUser);
         const hashSum = await versions.findAll({ raw: true });
+        const chave = crypto.createHash('sha1').update(hashSum[0].tablesChecksum + getUserCheckIntegrity.checksum).digest('hex');
         return res.status(200).json({
-            "horariosHash": hashSum[0].tablesChecksum
+            "hashIntegrity": chave
         });
     }
 };
